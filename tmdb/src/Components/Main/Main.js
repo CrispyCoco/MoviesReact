cd143a373d0147ce62d4319224f99a97e6b12b23
martinatriulzi <mtriulzi@udesa.edu.ar>
2021-09-19 12:40:17 -0300

    bebve

--- tmdb/src/Components/Main/Main.js
+++ tmdb/src/Components/Main/Main.js
@@ -10,10 +10,10 @@ class Main extends Component {
       topRated: null,
       initialMovies: null,
       page: 1,
-      action:false,
-      normal:true,
+      action: false,
+      normal: true,
       asc: false,
-      default: true
+      default: true,
     };
   }
 
@@ -36,27 +36,29 @@ class Main extends Component {
   remove(removed) {
     if (this.state.topRated) {
       let newList = this.state.topRated.filter((movie) => movie.id !== removed);
-      this.setState({ 
+      this.setState({
         topRated: newList,
-        initialMovies: newList, 
+        initialMovies: newList,
       });
     }
   }
 
   addMore() {
-    this.setState({
-      page: this.state.page + 1,
-    },()=>{
-      if (this.state.topRated) {
-        fetch(
-          "https://api.themoviedb.org/3/movie/top_rated?api_key=9dec800f789f21e12920e46fc4882908&page=" +
-            this.state.page
-        )
-          .then((response) => response.json())
-          .then((data) => {
-            console.log(data);
-            let newList = this.state.topRated.concat(data.results)
-              if(!this.state.asc && !this.state.default){
+    this.setState(
+      {
+        page: this.state.page + 1,
+      },
+      () => {
+        if (this.state.topRated) {
+          fetch(
+            "https://api.themoviedb.org/3/movie/top_rated?api_key=9dec800f789f21e12920e46fc4882908&page=" +
+              this.state.page
+          )
+            .then((response) => response.json())
+            .then((data) => {
+              console.log(data);
+              let newList = this.state.topRated.concat(data.results);
+              if (!this.state.asc && !this.state.default) {
                 this.setState({
                   topRated: newList.sort((a, b) => {
                     let nameA = a.title.toLowerCase();
@@ -72,7 +74,7 @@ class Main extends Component {
                   initialMovies: this.state.initialMovies.concat(data.results),
                   asc: false,
                 });
-              } else if(this.state.asc){
+              } else if (this.state.asc) {
                 this.setState({
                   topRated: newList.sort((a, b) => {
                     let nameA = a.title.toLowerCase();
@@ -83,22 +85,22 @@ class Main extends Component {
                     if (nameA > nameB) {
                       return 1;
                     }
-                    return 0;
+                      return 0;
                   }),
                   initialMovies: this.state.initialMovies.concat(data.results),
-                  asc: true
+                  asc: true,
                 });
-              } else{
+              } else {
                 this.setState({
                   topRated: newList,
                   initialMovies: this.state.initialMovies.concat(data.results),
-                })
+                });
               }
-             
-          })
-          .catch((error) => console.log(error));
+            })
+            .catch((error) => console.log(error));
+        }
       }
-    });
+    );
   }
 
   searching(searched) {
@@ -107,41 +109,40 @@ class Main extends Component {
         movie.title.toLowerCase().includes(searched.toLowerCase())
       );
       console.log(this.state.topRated);
-      if(searchedList.length === 0){
+      if (searchedList.length === 0) {
         this.setState({ topRated: null, action: true });
-      }else{
+      } else {
         this.setState({ topRated: searchedList, action: true });
       }
     }
   }
 
-
-  changeOrientation(){
-    if (this.state.normal){
-      this.setState({normal:false})
-    }else{
-      this.setState({normal:true})
+  changeOrientation() {
+    if (this.state.normal) {
+      this.setState({ normal: false });
+    } else {
+      this.setState({ normal: true });
     }
   }
 
-  changeOrder(){
+  changeOrder() {
     if (!this.state.asc) {
-        this.setState({
-          asc: true,
-          topRated: this.state.topRated.sort((a, b) => {
-            let nameA = a.title.toLowerCase();
-            let nameB = b.title.toLowerCase();
-            if (nameA < nameB) {
-              return -1;
-            }
-            if (nameA > nameB) {
-              return 1;
-            }
-            return 0;
-          }),
-          default: false
-        })
-    }else{
+      this.setState({
+        asc: true,
+        topRated: this.state.topRated.sort((a, b) => {
+          let nameA = a.title.toLowerCase();
+          let nameB = b.title.toLowerCase();
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+          return 0;
+        }),
+        default: false,
+      });
+    } else {
       this.setState({
         asc: false,
         topRated: this.state.topRated.sort((a, b) => {
@@ -155,26 +156,32 @@ class Main extends Component {
           }
           return 0;
         }),
-        default: false
-      })
+        default: false,
+      });
     }
   }
   render() {
     return (
       <>
-        <Header search={(searched)=> this.searching(searched)} change={()=> this.changeOrientation()} direction={this.state.normal} sort={()=> this.changeOrder()} asc={this.state.asc} />
-
+        <Header
+          search={(searched) => this.searching(searched)}
+          change={() => this.changeOrientation()}
+          direction={this.state.normal}
+          sort={() => this.changeOrder()}
+          asc={this.state.asc}
+        />
 
         <main className="container">
-
           <button
             type="button"
             onClick={() => this.addMore()}
-            className={`${!this.state.topRated ? "empty": "more-cards"}`}
+            className={`${!this.state.topRated ? "empty" : "more-cards"}`}
           >
             Load more cards
           </button>
-          <section className={this.state.normal?`card-container`: `changing-flex`}>
+          <section
+            className={this.state.normal ? `card-container` : `changing-flex`}
+          >
             {this.state.topRated ? (
               this.state.topRated.map((movie, idx) => (
                 <Card
@@ -183,14 +190,11 @@ class Main extends Component {
                   remove={(removed) => this.remove(removed)}
                   orientation={this.state.normal}
                 />
-
               ))
-            ) : (
-              !this.state.action?
+            ) : !this.state.action ? (
               <h2> Loading... </h2>
-              :
-              <h2>There are no results for your search
-              </h2>
+            ) : (
+              <h2>There are no results for your search</h2>
             )}
           </section>
         </main>
